6ed4afde47b768e2a3d200bae0c2154b43ea0df7
johnsonb2 <johnsonb2@users.noreply.github.com>
2018-04-28 15:53:22 -0700

    Added script to destroy objects when they go out of bounds

---
+++ FoodDudes/Assets/Scripts/OutOfBoundsDestroyer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * This script checks on every update if the
+ * object has gone out of bounds. If it has,
+ * the object is destroyed.
+ */
+[RequireComponent(typeof(BoxCollider2D))]
+public class OutOfBoundsDestroyer : MonoBehaviour {
+
+    void Update() {
+        // enemy data
+        Vector3 pos = transform.position;
+        Vector2 size = GetComponent<BoxCollider2D>().size;
+        // camera data
+        Camera camera = Camera.main;
+        Vector3 cameraPos = camera.transform.position;
+        float cameraWidth = camera.orthographicSize * camera.aspect * 2;
+        float cameraHeight = camera.orthographicSize * 2;
+        // destroy if outside the left, right, top, or bottom bounds
+        if ((pos.x - (size.x / 2) <= cameraPos.x - (cameraWidth / 2)) ||
+            (pos.x + (size.x / 2) >= cameraPos.x + (cameraWidth / 2)) ||
+            (pos.y + (size.y / 2) >= cameraPos.y + (cameraHeight / 2)) ||
+            (pos.y - (size.y / 2) <= cameraPos.y - (cameraHeight / 2))) {
+            Destroy(gameObject);
+        }
+    }
+}
