4bd93040c162433b8b6490b9367cc0e7e8290a4a
Matthew Sell <matthew.r.sell@gmail.com>
2018-04-28 16:17:34 -0700

    Boss, movement only. Not committing location of spawner yet.

---
+++ FoodDudes/Assets/Scripts/BossController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossController : MonoBehaviour {
+
+	// maximum speed for this BOSS
+	private float maxSpeed = 1.0f;
+
+	// a relative factor for setting agressiveness (from 0.0 to 1.0)
+	private float aggressiveness = 0.5f;
+
+	// the game object this player will attack
+	private GameObject objectToAttack;
+
+	// the Rigidbody 2D associated with this BOSS
+	private Rigidbody2D rigidBody;
+
+	// location of this BOSS
+	private Transform bossPosition;
+
+	// location of the PLAYER
+	private Transform playerPosition;
+
+	// initialization
+	void Start () {
+
+		rigidBody = GetComponent<Rigidbody2D> ();
+		bossPosition = GetComponent<Transform> ();
+		objectToAttack = GameObject.Find ("Player");
+		playerPosition = objectToAttack.transform;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void FixedUpdate() {
+
+		// set direction of movement towards player, based on agressiveness
+		float moveHorizonal = ( playerPosition.position.x - bossPosition.position.x ) * aggressiveness;
+		float moveVertical = ( playerPosition.position.y - bossPosition.position.y ) * aggressiveness;
+
+		// remaining movement not based on aggressiveness should be random
+		moveHorizonal += Random.value * ( 1.0f - aggressiveness );
+		moveVertical += Random.value * ( 1.0f - aggressiveness );
+
+		// random speed component
+		float speed = maxSpeed * Random.value;
+
+		// apply the movement
+		Vector2 movement = new Vector2 (moveHorizonal, moveVertical);
+		rigidBody.AddForce ( movement * speed );
+
+	}
+
+	public void setMaxSpeed( float speed ) {
+		maxSpeed = speed;
+	}
+
+	public void setAggressiveness( float aggressiveness ) {
+		this.aggressiveness = aggressiveness;
+	}
+
+}
---
+++ FoodDudes/Assets/Scripts/BossSpawner.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossSpawner : MonoBehaviour {
+
+	// spawn a BOSS when player is within this relative Y position
+	public int triggerDistance = 4;
+
+	// a relative factor for setting agressiveness (from 0.0 to 1.0)
+	public float aggressiveness = 0.5f;
+
+	// the type of object that will represent the BOSS
+	public GameObject bossType;
+
+	// the object that the BOSS will attack
+	public GameObject player;
+
+	// once spawned, the BOSS will have this maximum speed
+	public float maxSpeed = .4f;
+
+	// was the BOSS created?
+	private bool wasSpawned = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// time to spawn the BOSS?
+		if ( !wasSpawned && transform.position.y <= triggerDistance ) {
+
+			// unleash the BOSS!
+			spawnBoss();
+
+			// don't create more than one!
+			wasSpawned = true;
+
+		}
+
+	}
+
+	// instantiate the BOSS
+	void spawnBoss() {
+
+		// create an instance of the BOSS at the spawn location
+		GameObject boss = Instantiate( bossType, transform );
+		BossController bossController = boss.GetComponent<BossController>();
+
+		// set BOSS parameters
+		bossController.setMaxSpeed( maxSpeed );
+		bossController.setAggressiveness (aggressiveness);
+
+	}
+
+}
