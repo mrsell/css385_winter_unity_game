b01026656fde6b9c3739e9631c967842ffefd868
Matthew Sell <matthew.r.sell@gmail.com>
2018-05-13 22:54:35 -0700

    Initial ugly main menu and options menu

---
+++ FoodDudes/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public void PlayGame() {
+
+		// loading this scene starts the game
+		SceneManager.LoadScene ("TestScene");
+
+	}
+
+	public void QuitGame() {
+
+		// end the game
+		Application.Quit ();
+
+	}
+
+}
