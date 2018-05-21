849fbee83f41381f118f0c682a3900c37d4df212
Matthew Sell <matthew.r.sell@gmail.com>
2018-05-20 19:03:10 -0700

    Initial tutorial components, including UI and assets.

---
+++ FoodDudes/Assets/Scripts/Tutorial/TutorialController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialController : MonoBehaviour {
+
+	public string text;
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.name == "Player")
+		{
+			TurnOnMessage();
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.name == "Player")
+		{
+			TurnOffMessage();
+		}
+	}
+
+}
