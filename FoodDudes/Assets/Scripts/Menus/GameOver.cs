68daf3d350b16645241d45866f6a3642e12cbb68
Matthew Sell <matthew.r.sell@gmail.com>
2018-05-23 22:18:42 -0700

    Added final score display to GameOver scene

--- FoodDudes/Assets/Scripts/Menus/GameOver.cs
+++ FoodDudes/Assets/Scripts/Menus/GameOver.cs
@@ -15,6 +15,7 @@ public class GameOver : MonoBehaviour {
 	private Text hitsRegisteredTextObj;
 	private Text hitRateTextObj;
 	private Text winLoseTextObj;
+	private Text finalScoreTextObj;
 
 	private Stats stats = new Stats();
 
@@ -25,10 +26,12 @@ public class GameOver : MonoBehaviour {
 		hitsRegisteredGameObj = GameObject.Find ("HitsText");
 		hitRateGameObj = GameObject.Find ("AccuracyText");
 		winLoseGameObj = GameObject.Find ("WinLoseText");
-		foodThrownTextObj = foodThrownGameObj.GetComponent<Text> ();
+		//foodThrownTextObj = foodThrownGameObj.GetComponent<Text> ();
+		foodThrownTextObj = GameObject.Find ("FoodThrownText").GetComponent<Text> ();
 		hitsRegisteredTextObj = hitsRegisteredGameObj.GetComponent<Text> ();
 		hitRateTextObj = hitRateGameObj.GetComponent<Text> ();
 		winLoseTextObj = winLoseGameObj.GetComponent<Text> ();
+		finalScoreTextObj = GameObject.Find ("FinalScoreText").GetComponent<Text> ();
 
 		// set win/lose text
 		winLoseTextObj.text = stats.getEndText();
@@ -37,6 +40,9 @@ public class GameOver : MonoBehaviour {
 		foodThrownTextObj.text = "Food Thrown: " + stats.getShotsFired();
 		hitsRegisteredTextObj.text = "Hits: " + stats.getHitsRegistered();
 		hitRateTextObj.text = "Accuracy: " + stats.getShotAccuracy() + "%";
+
+		// final score
+		finalScoreTextObj.text = "Final Score: " + Score.score;
 
 	}
 
