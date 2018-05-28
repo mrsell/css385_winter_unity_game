0641b49bfb8e084b7ecae45dc3b62af2cbe1ac58
Matthew Sell <matthew.r.sell@gmail.com>
2018-05-27 21:01:22 -0700

    Added more tutorial text triggers, simplified TutorialController

--- FoodDudes/Assets/Scripts/Tutorial/TutorialController.cs
+++ FoodDudes/Assets/Scripts/Tutorial/TutorialController.cs
@@ -7,27 +7,9 @@ public class TutorialController : MonoBehaviour {
 
 	public string tutorialText;
 
-	private GameObject tutorialCanvas;
-	private GameObject tutorialTextComponent;
+	public GameObject tutorialCanvas;
+	public GameObject tutorialTextComponent;
 	private Text textObj;
-
-
-	void Start() {
-
-		tutorialCanvas = GameObject.Find ("TutorialCanvas");
-		tutorialTextComponent = GameObject.Find ("TutorialText");
-
-		// only set reference when canvas is active
-		if (tutorialTextComponent != null) {
-			textObj = tutorialTextComponent.GetComponent<Text> ();
-		}
-
-		// by default the tutorial should not be displayed
-		if (tutorialCanvas != null) {
-			tutorialCanvas.SetActive (false);
-		}
-
-	}
 
 	void OnTriggerEnter2D(Collider2D other) {
 
@@ -38,6 +20,7 @@ public class TutorialController : MonoBehaviour {
 			if (other.name == "Player") {
 
 				// set tutorial text
+				textObj = tutorialTextComponent.GetComponent<Text> ();
 				textObj.text = tutorialText;
 
 				// show the tutorial components
@@ -54,7 +37,7 @@ public class TutorialController : MonoBehaviour {
 
 		// are tutorials enabled?
 		if (RuntimeConfiguration.displayTutorialDialogs) {
-
+	
 			// only display tutorial text for the player
 			if (other.name == "Player") {
 				tutorialCanvas.SetActive (false);
